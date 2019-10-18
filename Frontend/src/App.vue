1df96b461611684e7dbd1160ecedd873183a1480
MuhaddiMu <Muhaddisshah@gmail.com>
2019-10-18 22:46:41 +0500

    :art: Create Toggle by Category

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,35 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- Apollo Query -->
+    <img alt="Vue logo" src="./assets/logo.png" />
     <ApolloQuery :query="require('./GraphQL/Queries/Categories.gql')">
       <!-- The result will automatically updated -->
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <!-- Some content -->
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          <span @click="UpdateSelectedCategory(Category.id)" v-for="(Category, index) of data.Categories" :key="Category.id" class="mr-25">
-            {{ index+1 }} - {{Category.name}}
-          </span>
+          <strong @click="UpdateSelectedCategory('All')" class="mr-25">All</strong>
+          <strong @click="UpdateSelectedCategory('Featured')" class="mr-25">Featured</strong>
+          <strong @click="UpdateSelectedCategory(Category.id)" v-for="Category of data.Categories" :key="Category.id" class="mr-25">
+            {{Category.name}}
+          </strong>
         </div>
       </template>
     </ApolloQuery>
 
+    <div v-if="SelectedCategory === 'All'">
+      <ApolloQuery :query="require('./GraphQL/Queries/AllBooks.gql')">
+        <!-- The result will automatically updated -->
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <!-- Some content -->
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+             <div v-for="(Book, index) in data.Books" :key="Book.id">
+              {{index+1}} - {{Book.title}}
+            </div> 
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
 
+     <div v-else-if="SelectedCategory === 'Featured'">
+      <ApolloQuery :query="require('./GraphQL/Queries/FeaturedBooks.gql')" :variables="{Featured: true}">
+        <!-- The result will automatically updated -->
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <!-- Some content -->
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="(Book, index) in data.FeaturedBooks" :key="Book.id">
+              {{index+1}} - {{Book.title}}
+            </div> 
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
+    <div v-else>
     <ApolloQuery :query="require('./GraphQL/Queries/Category.gql')" :variables="{id: SelectedCategory}">
       <!-- The result will automatically updated -->
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <!-- Some content -->
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          <div v-for="(Book, index) in data.Category.Books" :key="Book.id">
+           <div v-for="(Book, index) in data.Category.Books" :key="Book.id">
             {{index+1}} - {{Book.title}}
-          </div>
+          </div> 
         </div>
       </template>
     </ApolloQuery>
-
-
+    </div>
   </div>
 </template>
 
@@ -38,12 +68,18 @@ export default {
   name: 'app',
   data(){
     return{
-      SelectedCategory: "1"
+      SelectedCategory: "All"
     }
   },
   methods: {
     UpdateSelectedCategory(CategoryID){
-      this.SelectedCategory = CategoryID
+      if(CategoryID === 'Featured'){
+        this.SelectedCategory = CategoryID
+      } else if (CategoryID === 'All'){
+        this.SelectedCategory = CategoryID
+      } else {
+        this.SelectedCategory = CategoryID
+      }
     }
   },
 }
@@ -61,7 +97,8 @@ export default {
 
 .mr-25 {
   margin-right: 25px;
-  text-decoration: none
+  text-decoration: none;
+  cursor: pointer;
 }
 
 </style>
