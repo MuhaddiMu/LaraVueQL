1ad3612a482064cb15386c3d2b9389c26821fe43
MuhaddiMu <Muhaddisshah@gmail.com>
2019-10-19 20:25:41 +0500

    :zap: Delete Book Mutation

--- Frontend/src/Book.vue
+++ Frontend/src/Book.vue
@@ -8,23 +8,47 @@
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <!-- Some content -->
         <div v-if="isLoading">Loading...</div>
-        <div v-else>
+        <div v-else-if="data">
           <h2>{{ data.Book.title }} by {{ data.Book.author }}</h2>
           <img width="300" height="400" :src="`${data.Book.image}`" />
           <div>
             <router-link class="mr-25" :to="`Edit/${$route.params.ID}`"
               >Edit</router-link
             >
-            <router-link to="#" class="mr-25">Delete</router-link>
+            <a
+              to="#"
+              @click.prevent="DeleteBook($route.params.ID)"
+              class="mr-25"
+              >Delete</a
+            >
           </div>
         </div>
+        <div v-else-if="!data">Book Not Found</div>
       </template>
     </ApolloQuery>
   </div>
 </template>
 
 <script>
-export default {}
+import DeleteBook from "./GraphQL/Mutations/DeleteBook.gql"
+export default {
+  methods: {
+    DeleteBook(ID) {
+      this.$apollo
+        .mutate({
+          mutation: DeleteBook,
+          variables: { ID: ID }
+        })
+        .then(data => {
+          console.log(data)
+          this.$router.push("/")
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
 </script>
 
 <style scoped></style>
